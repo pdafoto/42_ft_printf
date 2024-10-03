87df5ddbe019a594fcf1e327c40a8ab58497f667
nperez-d <nperez-d@student.42madrid.com>
2024-10-03 13:18:11 +0200

    More tests

--- test.c
+++ test.c
@@ -6,22 +6,69 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 11:26:04 by nperez-d          #+#    #+#             */
-/*   Updated: 2024/10/03 12:01:45 by nperez-d         ###   ########.fr       */
+/*   Updated: 2024/10/03 13:15:29 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "ft_printf.h"
 
 int	main(void)
 {
-	int		number;
-	char	*str;
+	int				number;
+	char			*str;
+	unsigned int	uint;
+	void			*ptr;
+	char			c;
+	int				len;
 
-	number = 42;
-	str = "Hello, World!";
+	number = -42;
+	str = "Hello, Marvin!";
+	uint = 42;
+	ptr = (void *)&number;
+	c = 'T';
 
-	printf("Number: %d\nString: %s\n", number, str);
-	ft_printf("Number: %d\nString: %s\n", number, str);
+	len = printf("Original Simple test\n");
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Simple test\n");
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Character test (c): %c\n", c);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Character test (c): %c\n", c);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original String test (s): %s\n", str);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom String test (s): %s\n", str);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Pointer test (p): %p\n", str);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Pointer test (p): %p\n", str);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Integer test (d): %d\n", number);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Integer test (d): %d\n", number);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Integer test (i): %i\n", number);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Integer test (i): %i\n", number);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Unsigned Integer test (u): %u\n", uint);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Unsigned Integer test (u): %u\n", uint);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Hex test (x,X): %x - %X\n", number, number);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Hex test (x,X): %x - %X\n", number, number);
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Double percentage test:\n%% OK\n");
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Double percentage test:\n%% OK\n");
+	ft_printf("counter=%d\n", len);
+	len = printf("Original Null pointer test:\n%p\n", NULL);
+	printf("counter=%d\n", len);
+	len = ft_printf("Custom Null pointer test:\n%p\n", NULL);
+	ft_printf("counter=%d\n", len);
 	return (0);
 }
